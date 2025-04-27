11d576b9de8526b54ddba5598311c023a459bd2a
Lee Da-hyeon <dlekgus0205@naver.com>
2025-04-28 01:46:32 +0900

    feat: Type 엔티티 생성

---
+++ src/main/java/com/ecode/tabula_be/entity/Type.java
@@ -0,0 +1,32 @@
+package com.ecode.tabula_be.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Document("type")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Type {
+
+    @Id
+    private Long id;
+
+    @DBRef
+    private List<NoteType> noteType;
+
+    private String langType;
+
+    private String formType;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
