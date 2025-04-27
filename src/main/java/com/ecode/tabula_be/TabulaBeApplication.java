e9c626686917b4c529be4b618fcf4ae0abc89c18
Lee Da-hyeon <dlekgus0205@naver.com>
2025-04-28 01:45:28 +0900

    setting: 기본 세팅

---
+++ src/main/java/com/ecode/tabula_be/TabulaBeApplication.java
@@ -0,0 +1,13 @@
+package com.ecode.tabula_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabulaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabulaBeApplication.class, args);
+	}
+
+}
