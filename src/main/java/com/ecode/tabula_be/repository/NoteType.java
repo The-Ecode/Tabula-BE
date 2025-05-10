dd5bf71a9f340ac855d4ed7a88a983d4a2564b6e
Lee Da-hyeon <dlekgus0205@naver.com>
2025-05-10 21:42:00 +0900

    setting: NoteTypeRepository 파일명 변경

--- src/main/java/com/ecode/tabula_be/repository/NoteType.java
+++
@@ -1,7 +0,0 @@
-package com.ecode.tabula_be.repository;
-
-import com.ecode.tabula_be.entity.Folder;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NoteType extends MongoRepository<NoteType, String> {
-}
---
+++ src/main/java/com/ecode/tabula_be/repository/NoteTypeRepository.java
@@ -0,0 +1,6 @@
+package com.ecode.tabula_be.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NoteTypeRepository extends MongoRepository<NoteTypeRepository, String> {
+}
